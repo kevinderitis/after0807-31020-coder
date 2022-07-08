9ded164ebaea2a43cd8ac20b779d69e7aea855ac
kevinderitis <kevin.deritis@hotmail.com>
2022-07-08 20:09:22 -0300

    Ruta2 creada

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/ruta', (req, res) => {
     res.send('Hola mundo');
 })
 
+app.get('ruta2', (req, res) => {
+    res.send('Chau mundo');
+})
+
 app.listen(8080, () => console.log('Server running'))
